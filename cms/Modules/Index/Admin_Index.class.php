b4c7ec1a095d5034466fc7631e0e93e791a45a32
kamaikin <kamaikin@gmail.com>
2014-09-08 17:18:35 +0400

    Статистика в админке

--- cms/Modules/Index/Admin_Index.class.php
+++ cms/Modules/Index/Admin_Index.class.php
@@ -24,6 +24,22 @@
 			$query="SELECT count(`id`) as count FROM concept_comment WHERE `date` >?";
 			$SQL=\Tango::sql()->select($query, array($time));
 			$this->_view['data4']=$SQL[0]['count'];
+			//	Всего лайков
+			$query="SELECT count(`id`) as count FROM concept_licke";
+			$SQL=\Tango::sql()->select($query);
+			$this->_view['data5']=$SQL[0]['count'];
+			//	Лайков за последние сутки
+			$query="SELECT count(`id`) as count FROM concept_licke WHERE datetime >?";
+			$SQL=\Tango::sql()->select($query, array($time));
+			$this->_view['data6']=$SQL[0]['count'];
+			//	Всего пользователей
+			$query="SELECT count(`id`) as count FROM user_data";
+			$SQL=\Tango::sql()->select($query);
+			$this->_view['data7']=$SQL[0]['count'];
+			//	Пользователей за последние сутки
+			$query="SELECT count(`id`) as count FROM user_data WHERE register_date >?";
+			$SQL=\Tango::sql()->select($query, array($time));
+			$this->_view['data8']=$SQL[0]['count'];
 			$this->_view['includeFileName']='Index/index.tpl';
 		}
 	}
