14798dd6b490ab54e8551d7f3bc76822eaa1244e
kamaikin <kamaikin@gmail.com>
2014-09-08 16:20:36 +0400

    оставил две вкладки у чужого профиля

--- cms/Modules/User/Front_Index.class.php
+++ cms/Modules/User/Front_Index.class.php
@@ -41,7 +41,7 @@
 			$query = "SELECT * FROM concept WHERE user_id = ? ORDER BY `date` DESC";
 			$SQL = \Tango::sql()->select($query, array($user_info['user_id']));
 			$this->_view['user_concept']=$SQL;
-			if ($user_info['user_role']=='sponsor') {
+			/*if ($user_info['user_role']=='sponsor') {
 				//	Я спонсирую
 				$query="SELECT c.foto as foto, 
 					c.points as points, 
@@ -53,18 +53,18 @@
 					ud.surname as user_surname, cs.datetime as datetime  FROM concept c JOIN concept_sponsor cs ON cs.concept_id = c.id JOIN user_data ud ON ud.user_id = c.user_id WHERE cs.user_id=?";
 				$SQL = \Tango::sql()->select($query, array($user_info['user_id']));
 				$this->_view['ya_sponsor_concept']=$SQL;
-			}
+			}*/
 			//	Мне нравяться
-			$query="SELECT c.foto as foto, 
+			/*$query="SELECT c.foto as foto, 
 				c.points as points, 
 				c.post_like as post_like, 
 				c.comment_count as comment_count, 
 				c.name as name, c.date as `date`, cl.datetime as datetime FROM concept c JOIN concept_licke cl ON cl.concept_id = c.id WHERE cl.user_id = ? ORDER BY c.`date` DESC";
 			$SQL = \Tango::sql()->select($query, array($user_info['user_id']));
-			$this->_view['my_lacke_concept']=$SQL;
+			$this->_view['my_lacke_concept']=$SQL;*/
 			//	Меня спонсируют
 			//	Получить мои идеи и данные спонсоров...
-			$query="SELECT c.foto as foto, 
+			/*$query="SELECT c.foto as foto, 
 				c.points as points, 
 				c.post_like as post_like, 
 				c.comment_count as comment_count, 
@@ -73,11 +73,11 @@
 				ud.name as user_name,
 				ud.surname as user_surname  FROM concept c JOIN concept_sponsor cs ON cs.concept_id = c.id JOIN user_data ud ON ud.user_id = cs.user_id WHERE c.user_id=?";
 			$SQL = \Tango::sql()->select($query, array($user_info['user_id']));
-			$this->_view['my_sponsor_concept']=$SQL;
+			$this->_view['my_sponsor_concept']=$SQL;*/
 			$this->_view['includeFileName']='User/profile1.tpl';
 			//	Определяем, что изменилось с нашего последнего захода в профиль
 			$query="SELECT time FROM user_profile_views WHERE profile_id=? AND user_id=? ORDER BY time DESC LIMIT 1";
-			$SQL = \Tango::sql()->select($query, array($user_info['user_id'], $my_user_info['user_id']));
+			$SQL = \Tango::sql()->select($query, array($my_user_info['user_id'], $user_info['user_id']));
 			if ($SQL!=array()) {
 				$time=$SQL[0]['time'];
 			}else{
@@ -87,11 +87,11 @@
 			//	Теперь отмечаем вход пользователя
 			$array=array();
 			$array['user_id']=$user_info['user_id'];
-			$array['profile_id']=$user_info['user_id'];
+			$array['profile_id']=$my_user_info['user_id'];
 			$array['time']=time();
 			\Tango::sql()->insert('user_profile_views', $array);
 			//	Мне нравяться
-			$query="SELECT count(c.id) as count FROM concept c JOIN concept_licke cl ON cl.concept_id = c.id WHERE cl.user_id = ? AND cl.datetime>?";
+			/*$query="SELECT count(c.id) as count FROM concept c JOIN concept_licke cl ON cl.concept_id = c.id WHERE cl.user_id = ? AND cl.datetime>?";
 			$SQL = \Tango::sql()->select($query, array($user_info['user_id'], $time));
 			$this->_view['count_my_lacke_concept']=$SQL[0]['count'];
 			//	Меня спонсируют
@@ -103,7 +103,7 @@
 				$query="SELECT count(c.id) as count  FROM concept c JOIN concept_sponsor cs ON cs.concept_id = c.id JOIN user_data ud ON ud.user_id = c.user_id WHERE cs.user_id=? AND cs.datetime=?";
 				$SQL = \Tango::sql()->select($query, array($user_info['user_id'], $time));
 				$this->_view['count_ya_sponsor_concept']=$SQL[0]['count'];
-			}
+			}*/
 			//	Получить все мои комментарии
 			$query = "SELECT count(id) as count FROM concept_comment WHERE user_id = ?  AND `date`>?";
 			$SQL = \Tango::sql()->select($query, array($user_info['user_id'], $time));
